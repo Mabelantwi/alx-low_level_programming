13ec6b2e7b3b185c86eb4fd893f5db44441de7b2
Mabelantwi <Mabelantwi2016@gmail.com>
2023-03-24 03:46:37 -0700

    put betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
-* main - Entry point
+* main - Print with puts
 *
-* Return Always 0 (Sucess)
+* Return: 0 (Sucess)
 */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
